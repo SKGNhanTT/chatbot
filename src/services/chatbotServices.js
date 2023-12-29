9f066aac52625638b1e2e2f3babe668c8011fd5b
ThanhNhan <tn.nhan98@gmail.com>
2023-12-29 15:00:26 +0900

    Refactor callSendAPI function to accept response directly

--- src/services/chatbotServices.js
+++ src/services/chatbotServices.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const IMAGE_GET_STARTED =
     'https://cdn.bookingcare.vn/fo/2023/11/02/134537-group-12314.png';
-let callSendAPI = (sender_psid, { response }) => {
+let callSendAPI = (sender_psid, response) => {
     // Construct the message body
     let request_body = {
         recipient: {
             id: sender_psid,
         },
-        message: response,
+        message: { ...response },
     };
     console.log('check message', request_body.message);
 
